16fc3246d4daa53b1746f253c23afe3bdaddfc1d
Yohann Magnien <ymagnien@gmail.com>
2012-01-03 22:58:46 +0100

    v1.5beta(wip)

---
+++ modizer/uade-2.13/src/include/uadeconfig.h
@@ -0,0 +1,6 @@
+
+#define UADE_CONFIG_USER_MODE (0)
+#define UADE_CONFIG_BASE_DIR "/usr/local/share/uade2"
+#define UADE_CONFIG_UADE_CORE "/usr/local/lib/uade2/uadecore"
+#define UADE_CONFIG_HAVE_URANDOM
+#define UADE_VERSION "2.13"
