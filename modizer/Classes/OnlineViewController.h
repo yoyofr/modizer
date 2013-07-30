b7154fbc4298fce6397ed00e0425c9e8fd7ee0c2
Yohann Magnien <ymagnien@gmail.com>
2013-07-31 01:38:32 +0200

    add of new visufx in progress

---
+++ modizer/Classes/OnlineViewController.h
@@ -0,0 +1,13 @@
+//
+//  OnlineViewController.h
+//  modizer
+//
+//  Created by Yohann Magnien on 29/07/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OnlineViewController : UITableViewController
+
+@end
