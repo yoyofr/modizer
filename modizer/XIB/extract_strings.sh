dc1f9763d0dba52ccb45f8954668022f14976c7c
Yohann Magnien <ymagnien@gmail.com>
2013-08-04 08:02:17 +0200

    update in progress

--- modizer/XIB/extract_strings.sh
+++ modizer/XIB/extract_strings.sh
@@ -1,1 +1,2 @@
-ibtool --export-strings-file NIBCopy.string en.lproj/MainWindow.xib 
+ibtool --export-strings-file MainWindow_NIBCopy.string en.lproj/MainWindow.xib 
+ibtool --export-strings-file RootViewController_NIBCopy.string en.lproj/MainWindow.xib 
--- modizer/XIB/gen_fr.sh
+++ modizer/XIB/gen_fr.sh
@@ -1,1 +1,2 @@
-ibtool --import-strings-file fr.lproj/NIBCopy.string --write fr.lproj/MainWindow.xib en.lproj/MainWindow.xib 
+ibtool --import-strings-file fr.lproj/MainWindow_NIBCopy.string --write fr.lproj/MainWindow.xib en.lproj/MainWindow.xib 
+ibtool --import-strings-file fr.lproj/RootViewController_NIBCopy.string --write fr.lproj/RootViewController.xib en.lproj/RootViewcontroller.xib 
