e82a84b1a6f1b29f1ac018c7a37fe2b187b4a004
yoyofr <ymagnien@gmail.com>
2020-06-28 16:01:28 +0200

    added storyboard as launchscreen (new req from apple)

--- modizer/main.mm
+++ modizer/main.mm
@@ -7,20 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AppDelegate_Phone.h"
 #import "SettingsGenViewController.h"
+
 
 int main(int argc, char *argv[]) {
     
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    @autoreleasepool {
+        //***********************
+        //init stuff
+        //***********************
+        //settings
+        [SettingsGenViewController loadSettings];
+        [SettingsGenViewController restoreSettings];
+        
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate_Phone class]));
+
+    }
     
-    //***********************
-    //init stuff
-    //***********************
-    //settings
-    [SettingsGenViewController loadSettings];
-    [SettingsGenViewController restoreSettings];
+    //NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
-    int retVal = UIApplicationMain(argc, argv, nil, nil);
-    [pool release];
-    return retVal;
+    
+    //int retVal = UIApplicationMain(argc, argv, nil, nil);
+    //[pool release];
+    //return retVal;
 }
