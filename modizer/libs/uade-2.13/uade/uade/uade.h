405671aabb60c1bbad6119a4e1169023681dc2fd
yoyofr <ymagnien@gmail.com>
2021-02-07 09:37:14 +0100

    wip

---
+++ modizer/libs/uade-2.13/uade/uade/uade.h
@@ -0,0 +1,12 @@
+//
+//  uade.h
+//  uade
+//
+//  Created by Yohann Magnien on 07/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface uade : NSObject
+
+@end
---
+++ modizer/libs/uade-2.13/uade/uade/uade.m
@@ -0,0 +1,12 @@
+//
+//  uade.m
+//  uade
+//
+//  Created by Yohann Magnien on 07/02/2021.
+//
+
+#import "uade.h"
+
+@implementation uade
+
+@end
