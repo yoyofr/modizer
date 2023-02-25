0378f34ffb377d48bbbbb234c68694423706f07f
yoyofr <ymagnien@gmail.com>
2023-02-25 10:23:49 +0100

    preparing next update

--- src/ModizerConstants.h
+++ src/ModizerConstants.h
@@ -114,8 +114,8 @@
 
 #define VERSION_MAJOR 3
 #define VERSION_MAJOR_STR "3"
-#define VERSION_MINOR 2
-#define VERSION_MINOR_STR "2"
+#define VERSION_MINOR 3
+#define VERSION_MINOR_STR "3"
 
 
 #define MAX_PL_ENTRIES 65536
