0fadbbb213f3269254edd39d512b6a7af80f2dd1
yoyofr <ymagnien@gmail.com>
2025-10-15 01:00:56 +0200

    wip

--- utils/GLES3/MDZShaders/Fragment2DTexturesBlend.glsl
+++ utils/GLES3/MDZShaders/Fragment2DTexturesBlend.glsl
@@ -8,6 +8,7 @@ out vec4 outColor;
 uniform sampler2D u_textOriginal;
 uniform sampler2D u_textBlurred;
 //uniform float u_exposure;
+float u_exposure=1.0;
 
 void main()
 {
@@ -20,7 +21,7 @@ void main()
     hdrColor += bloomColor; // additive blending
     // tone mapping
     vec3 result;
-    float u_exposure=1.0;
+    
     result = hdrColor;
     result= vec3(1.0) - exp(-hdrColor * u_exposure);
     // also gamma correct while we're at it
--- utils/GLES3/MDZShaders/Fragment2DTexturesBlur.glsl
+++ utils/GLES3/MDZShaders/Fragment2DTexturesBlur.glsl
@@ -9,6 +9,8 @@ uniform sampler2D u_curTexture;
 uniform int u_horizontal;
 uniform float u_min_brightness;
 float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
+float boost_weight=1.0;
+float alpha_thres=0.02;
 
 void main()
 {
@@ -19,8 +21,8 @@ void main()
         for(int i = 1; i < 5; ++i)
         {
             float i_float=float(i);
-            result += texture(u_curTexture, v_textCoord + vec2(tex_offset.x * i_float, 0.0)).rgb * weight[i]*1.1;
-            result += texture(u_curTexture, v_textCoord - vec2(tex_offset.x * i_float, 0.0)).rgb * weight[i]*1.1;
+            result += texture(u_curTexture, v_textCoord + vec2(tex_offset.x * i_float, 0.0)).rgb * weight[i]*boost_weight;
+            result += texture(u_curTexture, v_textCoord - vec2(tex_offset.x * i_float, 0.0)).rgb * weight[i]*boost_weight;
         }
     }
     else
@@ -28,14 +30,13 @@ void main()
         for(int i = 1; i < 5; ++i)
         {
             float i_float=float(i);
-            result += texture(u_curTexture, v_textCoord + vec2(0.0, tex_offset.y * i_float)).rgb * weight[i]*1.1;
-            result += texture(u_curTexture, v_textCoord - vec2(0.0, tex_offset.y * i_float)).rgb * weight[i]*1.1;
+            result += texture(u_curTexture, v_textCoord + vec2(0.0, tex_offset.y * i_float)).rgb * weight[i]*boost_weight;
+            result += texture(u_curTexture, v_textCoord - vec2(0.0, tex_offset.y * i_float)).rgb * weight[i]*boost_weight;
         }
     }
     float alpha=length(result);
-    if (alpha>0.1) alpha=1.0;
+    if (alpha>alpha_thres) alpha=1.0;
 //    alpha=1.0;
     outColor = vec4(result, alpha);
-//    outColor = vec4(result, 1.0);
 }
 
